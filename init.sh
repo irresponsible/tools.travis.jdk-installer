4d677e814667eb895be857ffa3719dbbd4171134
Kent Fredric <kentfredric@gmail.com>
2017-04-13 01:45:52 +1200

    More tooly magic

--- init.sh
+++ init.sh
@@ -40,3 +40,18 @@ install_jdk() {
       ;;
   esac
 }
+
+install_boot() {
+  einfo "install_boot $@"
+  if [[ ! -f ./boot ]]; then
+    curl -fsSLo boot "https://github.com/boot-clj/boot-bin/releases/download/${BOOT_SH_VERSION:-2.5.2}/boot.sh" || return $?
+    chmod 755 boot || return $?
+  fi
+}
+
+setup_boot_env() {
+  set -x
+  export BOOT_VERSION="${BOOT_VERSION:-2.7.1}"
+  export BOOT_JVM_OPTIONS="-Xmx2g -client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none -XX:+CMSClassUnloadingEnabled"
+  set +x
+}
